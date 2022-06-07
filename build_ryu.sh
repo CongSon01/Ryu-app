8805b2e5d684517907651a3f7b621423fea84611
CongSon01 <duongcongson01@gmail.com>
2022-06-07 14:45:21 +0700

    edit sourc

--- build_ryu.sh
+++ build_ryu.sh
@@ -1,2 +1,2 @@
-source /home/onos/Ryu_controller/venv/bin/activate
+source /home/onos/Desktop/Ryu/venv/bin/activate
 ryu-manager --observe-link --ofp-tcp-listen-port=6633 --wsapi-port=8080 controller_rest.py ryu.app.ofctl_rest ryu.app.simple_switch_13
